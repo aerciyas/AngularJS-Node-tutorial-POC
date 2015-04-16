92010478b77e66a94d0d9c0d64d55e85301e1af5
aerciyas <aerciyas88@gmail.com>
2015-04-16 02:20:11 -0500

    Updating api base url.

--- src/frontend/app/eggly-app.start.js
+++ src/frontend/app/eggly-app.start.js
@@ -9,7 +9,7 @@
         'eggly.session'
     ]);
 
-        egglyApp.constant('API_URL', 'http://localhost:3000');
+        egglyApp.constant('API_URL', '//mysterious-earth-1808.herokuapp.com');
 
         egglyApp.config(function ($stateProvider, $urlRouterProvider) {
                 $stateProvider
