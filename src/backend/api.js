73986ac25aea521db3dad05f81faa04ca2c17ea4
aerciyas <aerciyas88@gmail.com>
2015-04-17 06:56:23 -0500

    Trying cors another way.

--- src/backend/api.js
+++ src/backend/api.js
@@ -8,11 +8,14 @@ var express = require('express'),
     passport = require('passport'),
     LocalStrategy = require('passport-local'),
     morgan = require('morgan'),             // log requests to the console (express4)
-    userAuth = require('./userAuth.js'); //userAuth file contains our helper functions for our Passport and database work
+    userAuth = require('./userAuth.js'), //userAuth file contains our helper functions for our Passport and database work
+    cors = require('cors');
+
 
 exports.api =  function() {
 
     var app = express();
+
 
     //===============PASSPORT===============
     // Passport session setup.
@@ -49,39 +52,40 @@ exports.api =  function() {
         }
     ));
 
-    //Use the LocalStrategy within Passport to register/"signup" users.
+     //Use the LocalStrategy within Passport to register/"signup" users.
 
-    passport.use('local-signup', new LocalStrategy(
-        {passReqToCallback : true}, //allows us to pass back the request to the callback
-        function(request, username, password, done)
-        {
-            userAuth.localReg(username, password)
-                .then(function success(user)
-                {
-                    if (user)
-                    {
-                        console.log("REGISTERED: " + user.username);
-                        request.session.success = 'You are successfully registered and logged in ' + user.username + '!';
-                        return done(null, user);
-                    }
-                    if (!user)
-                    {
-                        console.log("COULD NOT REGISTER");
-                        request.session.error = 'That username is already in use, please try a different one.'; //inform user could not log them in
-                        return done(null, user);
-                    }
-                })
-                .fail(function (err){ //TODO: For hangling promises, i see both this way ano the other way of handling them..
-                    console.log(err.body);
-                });
-        }
-    ));
+     passport.use('local-signup', new LocalStrategy(
+         {passReqToCallback : true}, //allows us to pass back the request to the callback
+         function(request, username, password, done)
+         {
+             userAuth.localReg(username, password)
+                 .then(function success(user)
+                     {
+                         if (user)
+                         {
+                             console.log("REGISTERED: " + user.username);
+                             request.session.success = 'You are successfully registered and logged in ' + user.username + '!';
+                             return done(null, user);
+                         }
+                         if (!user)
+                         {
+                             console.log("COULD NOT REGISTER");
+                             request.session.error = 'That username is already in use, please try a different one.'; //inform user could not log them in
+                             return done(null, user);
+                         }
+                    })
+                     .fail(function (err){ //TODO: For hangling promises, i see both this way ano the other way of handling them..
+                     console.log(err.body);
+                     });
+         }
+     ));
 
 
     //===============EXPRESS================
     // Configure Express
     var rootStaticDirectory = __dirname.replace("backend", "frontend");
     app.use(express.static(rootStaticDirectory));// set the static files location /public/img will be /img for users
+    app.use(cors());
     app.use(morgan('dev')); // log every request to the console
     app.use(methodOverride());
     app.use(logger('combined'));
@@ -91,20 +95,6 @@ exports.api =  function() {
     app.use(passport.session());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
-
-    //================CORS==================
-
-    app.use(function(req, res, next) {
-        res.header('Access-Control-Allow-Credentials', true);
-        res.header('Access-Control-Allow-Origin', req.headers.origin);
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-        if ('OPTIONS' == req.method) {
-            res.send(200);
-        } else {
-            next();
-        }
-    });
 
     // Configure express to use handlebars templates
     var hbs = exphbs.create({
@@ -124,7 +114,7 @@ exports.api =  function() {
     //sends the request through our local login/signin strategy, and if successful redirects to /userSession, otherwise stays in signin page
     app.post('/login',
         passport.authenticate('local-signin', { successRedirect: '/userSession'}
-        )
+            )
     );
 
     //sends the request through our local signup strategy, and if successful takes user to homepage, otherwise returns then to signin page
@@ -155,25 +145,25 @@ exports.api =  function() {
 
 
     /*
-     **
-     * Test to play with node api
-     *
+        **
+        * Test to play with node api
+        *
 
-     app.post('/test', function(request, response){
-     var name = request.body.name;
-     var body = request.body.body;
-     var session = request.session;
-     console.log("name: " + name);
-     console.log("body: " + body);
-     console.log("session: " + session);
-     console.log(request.session);
+         app.post('/test', function(request, response){
+             var name = request.body.name;
+             var body = request.body.body;
+             var session = request.session;
+             console.log("name: " + name);
+             console.log("body: " + body);
+             console.log("session: " + session);
+             console.log(request.session);
 
-     function test()
-     {
-     return 'hello world';
-     }
-     response.json({hello: test()});
-     });
+             function test()
+             {
+             return 'hello world';
+             }
+             response.json({hello: test()});
+         });
      */
 
 
